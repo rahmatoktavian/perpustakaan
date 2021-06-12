2588c1ee9fe28a4032c12e8d6c3f62f89677bf6f
rahmatoktavian <rahmatoktavian@gmail.com>
2021-06-12 08:39:35 +0700

    initapp

--- sql/user_access.sql
+++ sql/user_access.sql
@@ -9,8 +9,26 @@ INSERT INTO `user_type` (`id`, `nama`) VALUES
 (1,	'petugas'),
 (2,	'anggota');
 
-ALTER TABLE `user`
-ADD `user_type_id` int NOT NULL AFTER `id`;
+
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `user_type_id` int NOT NULL,
+  `type` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `petugas_id` int DEFAULT NULL,
+  `nim` int DEFAULT NULL,
+  `username` varchar(50) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  `password` varchar(255) CHARACTER SET latin1 COLLATE latin1_swedish_ci NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+INSERT INTO `user` (`id`, `user_type_id`, `type`, `petugas_id`, `nim`, `username`, `password`) VALUES
+(1, 1,  'petugas',  1,  NULL, 'rahmat', 'af2a4c9d4c4956ec9d6ba62213eed568'),
+(2, 1,  'petugas',  2,  NULL, 'inna', '18aa53c0ac2859deaca6674ee136809c'),
+(3, 2,  'anggota',  NULL, 100,  'dewi', 'ed1d859c50262701d92e5cbf39652792'),
+(4, 2,  'anggota',  NULL, 200,  'asad', '140b543013d988f4767277b6f45ba542');
+
+
 
 DROP TABLE IF EXISTS `akses`;
 CREATE TABLE `akses` (
